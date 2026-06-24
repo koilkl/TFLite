c86a025954e75482713e0e33361cc97026405bda
Koil <wcwobuxingle@outlook.com>
2026-06-24 14:49:13 +0800

    sample

---
+++ tm_model_data.h
@@ -0,0 +1,15 @@
+
+
+
+// This is a TensorFlow Lite model file that has been converted into a C data
+// array using the tensorflow.lite.util.convert_bytes_to_c_source() function.
+// This form is useful for compiling into a binary for devices that don't have a
+// file system.
+
+#ifndef G_TM_MODEL_DATA_H
+#define G_TM_MODEL_DATA_H
+
+extern const unsigned char g_tm_model_data[];
+extern const int g_tm_model_data_len;
+
+#endif  // G_TM_MODEL_DATA_H
